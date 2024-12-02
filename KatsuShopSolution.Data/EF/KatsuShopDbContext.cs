b68fd889051e137a064241a83e37e618b0363462
MrKatsu <mrk4tsu@gmail.com>
2024-12-02 18:16:01 +0700

    Add Entity

---
+++ KatsuShopSolution.Data/EF/KatsuShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using KatsuShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KatsuShopSolution.Data.EF
+{
+    public class KatsuShopDbContext : DbContext
+    {
+        public KatsuShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+
+}
