ecebf897306a17c62507b417caab6dfba31f36fa
MrKatsu <mrk4tsu@gmail.com>
2024-12-02 15:50:56 +0700

    Create Solution Structrure

---
+++ KatsuShopSolution.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KatsuShopSolution.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
