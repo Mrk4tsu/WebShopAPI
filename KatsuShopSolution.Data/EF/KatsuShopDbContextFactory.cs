55925d4e9083658f0c5b9dd10ee66bf31c54865a
MrKatsu <mrk4tsu@gmail.com>
2024-12-03 15:40:57 +0700

    Migration Database, cập nhật database

---
+++ KatsuShopSolution.Data/EF/KatsuShopDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace KatsuShopSolution.Data.EF
+{
+    public class KatsuShopDbContextFactory : IDesignTimeDbContextFactory<KatsuShopDbContext>
+    {
+        //Cài đặt Microsoft.Extensions.Configuration.FileExtensions
+        //và Microsoft.Extensions.Configuration.Json để đọc file appsettings.json
+        public KatsuShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("MrKatsuShopDatabase");
+            var optionBuilder = new DbContextOptionsBuilder<KatsuShopDbContext>();
+            optionBuilder.UseSqlServer(connectionString);
+
+            return new KatsuShopDbContext(optionBuilder.Options);
+        }
+    }
+}
