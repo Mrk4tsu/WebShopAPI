4b6c371ab6475785243d703b9a5ca243bad8cce0
MrKatsu <mrk4tsu@gmail.com>
2024-12-03 17:16:23 +0700

    Seeding Data cho database

---
+++ KatsuShopSolution.Data/Extensions/ModelBuilderExtension.cs
@@ -0,0 +1,187 @@
+﻿using KatsuShopSolution.Data.Entities;
+using KatsuShopSolution.Data.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatsuShopSolution.Data.Extensions
+{
+    public static class ModelBuilderExtension
+    {
+        public static void Seed(this ModelBuilder builder)
+        {
+            builder.Entity<AppConfig>().HasData(
+                    new AppConfig()
+                    {
+                        Key = "HomeTitle",
+                        Value = "This is home page of KatsuShop"
+                    },
+                    new AppConfig()
+                    {
+                        Key = "HomeKeyword",
+                        Value = "This is keyword of KatsuShop"
+                    },
+                    new AppConfig()
+                    {
+                        Key = "HomeDescription",
+                        Value = "This is description of KatsuShop"
+                    }
+                );
+            builder.Entity<Language>().HasData(
+                    new Language()
+                    {
+                        Id = "vi-VN",
+                        Name = "Tiếng Việt",
+                        IsDefault = true
+                    },
+                    new Language()
+                    {
+                        Id = "en-US",
+                        Name = "English",
+                        IsDefault = false
+                    }
+                );
+            builder.Entity<Category>().HasData(
+                    new Category()
+                    {
+                        Id = 1,
+                        IsShowOnHome = true,
+                        Name = "Category 1",
+                        SortOrder = 1,
+                        Status = Status.Active,
+                        ParentId = null
+                    },
+                    new Category()
+                    {
+                        Id = 2,
+                        IsShowOnHome = true,
+                        Name = "Category 2",
+                        SortOrder = 2,
+                        Status = Status.Active,
+                        ParentId = null
+                    }
+                );
+            builder.Entity<CategoryTranslation>().HasData(
+                   new CategoryTranslation()
+                   {
+                       Id = 1,
+                       CategoryId = 1,
+                       LanguageId = "vi-VN",
+                       Name = "Danh mục 1",
+                       SeoAlias = "danh-muc-1",
+                       SeoDescription = "Danh mục 1",
+                       SeoTitle = "Danh mục 1"
+                   },
+                   new CategoryTranslation()
+                   {
+                       Id = 2,
+                       CategoryId = 1,
+                       LanguageId = "en-US",
+                       Name = "Category 1",
+                       SeoAlias = "category-1",
+                       SeoDescription = "Category 1",
+                       SeoTitle = "Category 1"
+                   },
+                   new CategoryTranslation()
+                   {
+                       Id = 3,
+                       CategoryId = 2,
+                       LanguageId = "vi-VN",
+                       Name = "Danh mục 2",
+                       SeoAlias = "danh-muc-2",
+                       SeoDescription = "Danh mục 2",
+                       SeoTitle = "Danh mục 2"
+                   },
+                   new CategoryTranslation()
+                   {
+                       Id = 4,
+                       CategoryId = 2,
+                       LanguageId = "en-US",
+                       Name = "Category 2",
+                       SeoAlias = "category-2",
+                       SeoDescription = "Category 2",
+                       SeoTitle = "Category 2"
+                   }
+               );
+            builder.Entity<Product>().HasData(
+                    new Product()
+                    {
+                        Id = 1,
+                        DateCreated = DateTime.UtcNow,
+                        OriginalPrice = 100000,
+                        Price = 120000,
+                        Stock = 0,
+                        Name = "Product 1",
+                        ViewCount = 0,
+                        Description = "Chi tiết sản phẩm 1",
+                        Status = Status.Active,
+                    },
+                    new Product()
+                    {
+                        Id = 2,
+                        DateCreated = DateTime.UtcNow,
+                        OriginalPrice = 150000,
+                        Price = 180000,
+                        Stock = 0,
+                        Name = "Sản phẩm 2",
+                        ViewCount = 0,
+                        Description = "Chi tiết sản phẩm 2",
+                        Status = Status.Active,
+                    }
+                );
+            builder.Entity<ProductTranslation>().HasData(
+                    new ProductTranslation()
+                    {
+                        Id = 1,
+                        ProductId = 1,
+                        LanguageId = "vi-VN",
+                        Name = "Sản phẩm 1",
+                        SeoTitle = "Sản phẩm 1",                        
+                        SeoAlias = "san-pham-1",
+                        SeoDescription = "Đây là thông tin sản phẩm 1",
+                        Details = "Đây là chi tiết sản phẩm 1",                   
+                    },
+                    new ProductTranslation()
+                    {
+                        Id = 2,
+                        ProductId = 1,
+                        LanguageId = "en-US",
+                        Name = "Product 1",
+                        SeoTitle = "Product 1",
+                        SeoAlias = "product-1",
+                        SeoDescription = "This is description of product 1",                     
+                        Details = "This is detail of product 1",
+                    },
+                    new ProductTranslation()
+                    {
+                        Id = 3,
+                        ProductId = 2,
+                        LanguageId = "vi-VN",
+                        Name = "Sản phẩm 2",
+                        SeoTitle = "Sản phẩm 2",
+                        SeoAlias = "san-pham-2",
+                        Details = "Đây là chi tiết sản phẩm 2",
+                        SeoDescription = "Đây là thông tin sản phẩm 2",
+                    },
+                    new ProductTranslation()
+                    {
+                        Id = 4,
+                        ProductId = 2,
+                        LanguageId = "en-US",
+                        Name = "Product 2",
+                        SeoTitle = "Product 2",
+                        SeoAlias = "product-2",
+                        SeoDescription = "This is description of product 2",
+                        Details = "This is detail of product 2",
+                    }
+                );
+            builder.Entity<ProductInCategory>().HasData(
+                    new ProductInCategory() { ProductId = 1, CategoryId = 1 },
+                    new ProductInCategory() { ProductId = 2, CategoryId = 2 }
+                );
+        }
+    }
+}
---
+++ KatsuShopSolution.Data/Migrations/20241203101352_SeedData.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KatsuShopSolution.Data.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "ProductTranslations");
+
+            migrationBuilder.DropColumn(
+                name: "SeoAlias",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SeoAlias",
+                table: "Categories");
+
+            migrationBuilder.InsertData(
+                table: "AppConfigs",
+                columns: new[] { "Key", "Value" },
+                values: new object[,]
+                {
+                    { "HomeTitle", "This is home page of KatsuShop" },
+                    { "HomeKeyword", "This is keyword of KatsuShop" },
+                    { "HomeDescription", "This is description of KatsuShop" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "IsShowOnHome", "Name", "ParentId", "SortOrder", "Status" },
+                values: new object[,]
+                {
+                    { 1, true, "Category 1", null, 1, 1 },
+                    { 2, true, "Category 2", null, 2, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Languages",
+                columns: new[] { "Id", "IsDefault", "Name" },
+                values: new object[,]
+                {
+                    { "vi-VN", true, "Tiếng Việt" },
+                    { "en-US", false, "English" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "DateCreated", "Description", "Name", "OriginalPrice", "Price", "Status" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 12, 3, 10, 13, 52, 430, DateTimeKind.Utc).AddTicks(1144), "Chi tiết sản phẩm 1", "Product 1", 100000m, 120000m, 1 },
+                    { 2, new DateTime(2024, 12, 3, 10, 13, 52, 430, DateTimeKind.Utc).AddTicks(2972), "Chi tiết sản phẩm 2", "Sản phẩm 2", 150000m, 180000m, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "CategoryTranslations",
+                columns: new[] { "Id", "CategoryId", "LanguageId", "Name", "SeoAlias", "SeoDescription", "SeoTitle" },
+                values: new object[,]
+                {
+                    { 1, 1, "vi-VN", "Danh mục 1", "danh-muc-1", "Danh mục 1", "Danh mục 1" },
+                    { 3, 2, "vi-VN", "Danh mục 2", "danh-muc-2", "Danh mục 2", "Danh mục 2" },
+                    { 2, 1, "en-US", "Category 1", "category-1", "Category 1", "Category 1" },
+                    { 4, 2, "en-US", "Category 2", "category-2", "Category 2", "Category 2" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductInCategories",
+                columns: new[] { "CategoryId", "ProductId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductTranslations",
+                columns: new[] { "Id", "Details", "LanguageId", "Name", "ProductId", "SeoAlias", "SeoDescription", "SeoTitle" },
+                values: new object[,]
+                {
+                    { 1, "Đây là chi tiết sản phẩm 1", "vi-VN", "Sản phẩm 1", 1, "san-pham-1", "Đây là thông tin sản phẩm 1", "Sản phẩm 1" },
+                    { 2, "This is detail of product 1", "en-US", "Product 1", 1, "product-1", "This is description of product 1", "Product 1" },
+                    { 3, "Đây là chi tiết sản phẩm 2", "vi-VN", "Sản phẩm 2", 2, "san-pham-2", "Đây là thông tin sản phẩm 2", "Sản phẩm 2" },
+                    { 4, "This is detail of product 2", "en-US", "Product 2", 2, "product-2", "This is description of product 2", "Product 2" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeDescription");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeKeyword");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeTitle");
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "ProductInCategories",
+                keyColumns: new[] { "CategoryId", "ProductId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductInCategories",
+                keyColumns: new[] { "CategoryId", "ProductId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductTranslations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ProductTranslations",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ProductTranslations",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "ProductTranslations",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: "en-US");
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: "vi-VN");
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "ProductTranslations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SeoAlias",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SeoAlias",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
